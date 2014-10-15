9315cb44cd369decafe8161b56ac89bede6d12a5
shahpoint2 <jay.shah@lrapassociation.com>
2014-10-15 16:15:37 -0400

    Init ImportStudentsDocsER

---
+++ ImportStudentsDocsER/ImportStudentsDocsER/ImportStudentsDocsER/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="b3e88d75-69c8-4693-a887-4bfa47426d68" featureId="b3e88d75-69c8-4693-a887-4bfa47426d68" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ImportStudentsDocsER Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="a120c5f5-417a-4a77-8ced-7048763e5720" />
+  </projectItems>
+</feature>
---
+++ ImportStudentsDocsER/ImportStudentsDocsER/ImportStudentsDocsER/ImportStudentDocs/ImportStudentDocs.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace ImportStudentsDocsER.ImportStudentDocs
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class ImportStudentDocs : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item is being added.
+        /// </summary>
+        public override void ItemAdding(SPItemEventProperties properties)
+        {
+            base.ItemAdding(properties);
+            
+            //string str = "Adding1";
+
+            //SPListItem item = properties.ListItem;
+            //item["Title2"] = str;
+
+
+            //item.SystemUpdate(false);
+
+
+        }
+
+        /// <summary>
+        /// An item is being updated.
+        /// </summary>
+        //public override void ItemUpdating(SPItemEventProperties properties)
+        //{
+        //    base.ItemUpdating(properties);
+
+
+
+
+        //}
+
+        /// <summary>
+        /// An item is being checked in.
+        /// </summary>
+        //public override void ItemCheckingIn(SPItemEventProperties properties)
+        //{
+        //    base.ItemCheckingIn(properties);
+
+
+            
+        //}
+
+        /// <summary>
+        /// An item was added.
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdded(properties);
+               
+            
+            try
+                {
+                    using (SqlConnection connection = new SqlConnection("Server=lrapdev2013.cloudapp.net;Database=LRAP;UID=sa;Pwd=Lisa needs braces.@"))
+                    {
+                        connection.Open();
+                        string query = "select STUDENT_ID from STUDENT where FIRST_NAME='{0}' AND LAST_NAME='{1}'";
+
+                         SPListItem item = properties.ListItem;
+                         List<string> studentIds = new List<string>();
+                         string value = "";
+
+                        string fileName = "" + item["FileLeafRef"];
+
+                        var fileNameSplitArray = fileName.Split(new string[] { " " }, StringSplitOptions.None);
+
+                        int indexOfFirst = fileNameSplitArray.Length - 3;
+                        int indexOfLast = fileNameSplitArray.Length - 2;
+
+                        string firstName = fileNameSplitArray[indexOfFirst];
+                        string lastName = fileNameSplitArray[indexOfLast];
+
+
+
+
+                        string newQuery = String.Format(query, firstName, lastName);
+
+                        // Create the Command which will be used to obtain query answer
+                        SqlCommand command = new SqlCommand(newQuery, connection);
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            studentIds.Add("" + reader["STUDENT_ID"]);
+                        }
+
+                        if (studentIds.Count == 1)
+                        {
+                            value = studentIds[0];
+                        }
+                        //string rows =  "|" +   reader.RecordsAffected.ToString() + "|";
+                        
+            //
+                        item["studentId"] = value;
+                        item["Title"] = "Item Added: ImportStudentDocs Running";
+                        this.EventFiringEnabled = false;
+
+                        item.Update();
+
+                        this.EventFiringEnabled = true;
+
+
+                    }
+                   
+           
+
+            }
+            catch (Exception ex)
+            {
+                SPListItem item = properties.ListItem;
+                item["Title"] = "ERROR: " + ex.Message;
+                this.EventFiringEnabled = false;
+
+                item.Update();
+
+                this.EventFiringEnabled = true;
+                //item.SystemUpdate(false);
+
+            }
+
+            //item.SystemUpdate(false);
+        }
+
+        /// <summary>
+        /// An item was updated.
+        /// </summary>
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+
+
+
+
+
+            }
+           
+        /// <summary>
+        /// An item was checked in.
+        /// </summary>
+        public override void ItemCheckedIn(SPItemEventProperties properties)
+        {
+            base.ItemCheckedIn(properties);
+
+            //string str = "Adding6";
+
+            //SPListItem item = properties.ListItem;
+            //item["Title2"] = str;
+
+
+            //item.SystemUpdate(false);
+        }
+
+
+    }
+}
